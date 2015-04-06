e768bdc8e298f80488302f3677029c66542fbc46
nmgtn <andy@nmgtn.co.uk>
2015-04-06 21:56:33 +0100

    Tidying up a bit before merge into master branch

--- WASDAAT.c
+++ WASDAAT.c
@@ -9,20 +9,20 @@
 #include "portsf.h"
 #include <unistd.h> // For access()
 
-/*===== Pre-processor defines =================================================================*/
+/*===== Pre-processor defines ============================*/
 
 #define NUM_SAMPLES_IN_FRAME 1024
 #define FILE_SAMPLE_FREQ 44100.0
 // This ensures that we don't close a file in portsf that wasn't opened in the first place!
 #define INVALID_PORTSF_FID -1
 
-/*===== Enums for portsf =================================================================*/
+/*===== Enums for portsf =================================*/
 
 enum float_clipping { DO_NOT_CLIP_FLOATS, CLIP_FLOATS };
 enum minheader { DO_NOT_MINIMISE_HDR, MINIMISE_HDR };
 enum auto_rescale { DO_NOT_AUTO_RESCALE, AUTO_RESCALE };
 
-/*===== List of functions =================================================================*/
+/*===== Function prototypes ==============================*/
 
 int multiToMonos();
 
@@ -93,20 +93,22 @@ int main(int argc, char *argv[]) {
     }
 
     // Do some stuff depending on what option has been chosen by the user
-    switch(operationToDo){
-    case 1:{
+    switch(operationToDo) { //todo We should use enums here instead of magic numbers
+    case 1:
     	if(multiToMonos(inputFilename) != EXIT_SUCCESS){
     		return EXIT_FAILURE;
     	}
-    }
     	break;
-    	case 2:
-    		printf("We haven't actually coded this functionality yet, please come back later\n");
-    		return EXIT_FAILURE;
-
+	case 2:
+		printf("We haven't actually coded this functionality yet, please come back later\n");
+		return EXIT_FAILURE;
 		break;
+    default:
+        printf("Error: no action assigned to that operation yet. This is "
+                "probably a coding error - contact the developer.\n");
     }
 
+    // Once case has executed...
     printf("The files have been processed. Thank you for using WASDAAT\n");
 
 
